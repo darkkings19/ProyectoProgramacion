8fecb421812e7a804aae74a29ea031b4add4227d
estebanSaez <85312860+estebansaez@users.noreply.github.com>
2022-11-29 13:28:04 -0300

    Delete Proyecto UFRO-STORE directory

--- Proyecto UFRO-STORE/src/main/java/Producto.java
+++
@@ -1,8 +0,0 @@
-public class Producto {
-
-    public static String rutaP = "productos/productos.txt";
-
-    public static void agregarProducto(){
-        GestorDatos.crearArchivoProductos(rutaP,IngresoDatos.infoProducto());
-    }
-}
